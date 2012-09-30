0b35aab5fc1c3c759b6b18e3ba8220906a77957d
Nick Jennings <nick@silverbucket.net>
2012-09-30 15:17:25 +0200

    remove loading text from console.js

--- console.js
+++ console.js
@@ -5,7 +5,6 @@
     var sys = require('sys');
     var always = require('./lib/always.js');
 
-console.log(always);
     var len_files = files.length;
     for (var i = 0; i < len_files; i++) {
         var s = require(files[0]);
@@ -19,7 +18,7 @@ console.log(always);
 
         var num_suites = suites.length;
         for (var n = 0; n < num_suites; n++) {
-            sys.print('...'+ (n+1));
+            //sys.print('...'+ (n+1));
             if (! always.loadSuite(suites[n]) ) {
                 console.error("\n"+'unable to load file: ' + files[i]);
                 console.error(always.getErrorMessage());
