c474430cee6a0812436159610d608e935c866865
Nick Jennings <nick@silverbucket.net>
2012-11-27 15:11:11 +0100

    formatting and syntax adaptions to improve readability, and to formalize on some standard conventions to be more consistent

--- test/66-websocket-suite.js
+++ test/66-websocket-suite.js
@@ -1,91 +1,91 @@
 if (typeof define !== 'function') {
   var define = require('amdefine')(module);
 }
-define([], function() {
-var suites = [];
+define([], function () {
+  var suites = [];
 
-suites.push({
-  name: "WebSocket tests",
-  desc: "collection of tests to test WebSocket communication",
-  setup: function(env) {
-    env.expected = { // struct of expected results for each http call
-      setupTest: 'setupTest',
-      test: {
-        foo: "bar"
-      },
-      footwear: {
-        leather: "boots",
-        flip: "flops",
-        block: "of wood"
-      },
-      complex: {
-        we: "are",
-        using: [ "a", "complex", "data"],
-        struct: [
-          {
-            here: "because",
-            it: ['makes', 'us', {feel: "better"}, "about"],
-          },
-          "things"
-        ]
-      }
-    };
+  suites.push({
+    name: "WebSocket tests",
+    desc: "collection of tests to test WebSocket communication",
+    setup: function (env) {
+      env.expected = { // struct of expected results for each http call
+        setupTest: 'setupTest',
+        test: {
+          foo: "bar"
+        },
+        footwear: {
+          leather: "boots",
+          flip: "flops",
+          block: "of wood"
+        },
+        complex: {
+          we: "are",
+          using: [ "a", "complex", "data"],
+          struct: [
+            {
+              here: "because",
+              it: ['makes', 'us', {feel: "better"}, "about"],
+            },
+            "things"
+          ]
+        }
+      };
 
-    var client = new this.WebSocketClient({
-      url: 'ws://localhost:9992/',
-      type: 'echo-protocol',
-      messages: env.expected // used for auto verification (if specified
-                   // by using the sendAndVerify() method in the
-                   // test).
-    });
-    var server = new this.WebSocketServer({
-      port: 9992,
-      messages: env.expected
-    });
-
-    var _this = this;
-    server.run(function() {
-      _this.write('websocket dummy server running');
-      // setup client
-      client.connect(function(connection) {
-        env.connection = connection;
-        env.connection.sendAndVerify('setupTest', _this);
+      var client = new this.WebSocketClient({
+        url: 'ws://localhost:9992/',
+        type: 'echo-protocol',
+        messages: env.expected // used for auto verification (if specified
+                     // by using the sendAndVerify() method in the
+                     // test).
       });
-    });
-  },
-  tests: [
-    {
-      desc: "first test",
-      run: function(env) {
-        this.assertAnd(env.connection.connected, true);
-        env.connection.sendAndVerify('test', this);
-      }
-    },
-    {
-      desc: "complex data struct",
-      run: function(env) {
-        env.connection.sendAndVerify('complex', this);
-      }
-    },
-    {
-      desc: "with callback",
-      run: function(env) {
-        var _this = this;
-        env.connection.sendWithCallback('footwear', function(data) {
-          _this.assert(data.utf8Data,
-                 JSON.stringify(env.expected['footwear']));
-        });
-      }
-    },
-    {
-      desc: 'lets try to fail! how exciting!',
-      willFail: true,
-      run: function(env) {
-        env.connection.sendAndVerify('dontexist', this);
-      }
-    }
-  ]
-});
+      var server = new this.WebSocketServer({
+        port: 9992,
+        messages: env.expected
+      });
 
-return suites;
+      var _this = this;
+      server.run(function () {
+        _this.write('websocket dummy server running');
+        // setup client
+        client.connect(function (connection) {
+          env.connection = connection;
+          env.connection.sendAndVerify('setupTest', _this);
+        });
+      });
+    },
+    tests: [
+      {
+        desc: "first test",
+        run: function (env) {
+          this.assertAnd(env.connection.connected, true);
+          env.connection.sendAndVerify('test', this);
+        }
+      },
+      {
+        desc: "complex data struct",
+        run: function (env) {
+          env.connection.sendAndVerify('complex', this);
+        }
+      },
+      {
+        desc: "with callback",
+        run: function (env) {
+          var _this = this;
+          env.connection.sendWithCallback('footwear', function (data) {
+            _this.assert(data.utf8Data,
+                   JSON.stringify(env.expected['footwear']));
+          });
+        }
+      },
+      {
+        desc: 'lets try to fail! how exciting!',
+        willFail: true,
+        run: function (env) {
+          env.connection.sendAndVerify('dontexist', this);
+        }
+      }
+    ]
+  });
+
+  return suites;
 });
